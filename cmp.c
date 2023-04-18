9c994fe2e9a4e80f0eaee60056cca4f637733342
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-04-18 12:06:40 +0300

    Update cmp.c

--- cmp.c
+++ cmp.c
@@ -1,53 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 /* 
 Matala 2 in Operating systems
  by ilan Souffir and Nerya Rez
 */
+int main(int argc, char *argv[]) 
+{
 
-int main(int argc, char* argv[]) {
-    int fd1, fd2, n;
-    char buffer[1024];
+    bool verb = false;
+    bool ign = false;
 
-    if (argc < 3) {
-        printf("Usage: copy <file1> <file2> [-v] [-f]\n");
+    if (argc < 3 || argc > 5) 
+    {
+        printf("Usage: cmp <file1> <file2> [-v] [-i]\n");
         return 1;
     }
 
-    if (access(argv[1], F_OK) == -1) {
-        printf("File %s does not exist\n", argv[1]);
+    for(int i=3; i<argc; i++)
+    {
+        if(strcmp(argv[i], "-v")==0)
+            verb = true;
+
+        else if (strcmp(argv[i], "-i")==0)
+            ign = true;
+    }
+
+    FILE *f1 = fopen(argv[1], "r");
+    
+    if (f1 == NULL)
+    {
+        printf("File1 error");
         return 1;
     }
 
-    if (access(argv[2], F_OK) != -1 && !strcmp(argv[3], "-f") == 0) {
-        printf("Target file exists. Use -f flag to force overwrite.\n");
+    FILE *f2 = fopen(argv[2], "r");
+
+    if (f2 == NULL)
+    {
+        printf("File2 error");
         return 1;
     }
 
-    fd1 = open(argv[1], O_RDONLY);
-    fd2 = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0666);
+    fseek(f1, 0, SEEK_END);
+    fseek(f2, 0, SEEK_END);
 
-    if (fd1 == -1 || fd2 == -1) {
-        printf("General failure\n");
+    long f1_len = ftell(f1);
+    long f2_len = ftell(f2);
+
+    rewind(f1);
+    rewind(f2);
+
+    if(f1_len != f2_len)
+    {
+        if(verb) printf("distinct\n");
         return 1;
     }
 
-    while ((n = read(fd1, buffer, sizeof(buffer))) > 0) {
-        write(fd2, buffer, n);
+    char c1, c2;
+    while ((c1 = fgetc(f1)) != EOF && (c2 = fgetc(f2)) != EOF)
+    {
+        if(ign)
+        {
+           c1 = tolower(c1);
+           c2 = tolower(c2);
+        }
+
+        if (c1 != c2)
+        {
+            if(verb) printf("distinct\n"); 
+            return 1; 
+        }
     }
-
-    close(fd1);
-    close(fd2);
-
-    if (strcmp(argv[3], "-v") == 0) {
-        printf("Success\n");
-    }
-
+    if(verb) printf ("equal\n");
+    
+    fclose(f1);
+    fclose(f2);
+ 
     return 0;
 }
 
