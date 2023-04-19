09970010e49b88b05d12dab05d1d9f09868d688b
neryaRez <121443102+neryaRez@users.noreply.github.com>
2023-04-19 16:31:45 +0300

    Update codecB.c

--- codecB.c
+++ codecB.c
@@ -1,4 +1,33 @@
+#include <ctype.h>
+#include <stdio.h>
+
 /* 
 Matala 2 in Operating systems
  by ilan Souffir and Nerya Rez
 */
+
+void encode(char *input) {
+
+    char output[1024] = {'\0'};
+    char * p = output;
+
+    while (*input) {
+        *p = *input + 3;
+        input++;
+        p++;
+    }
+    puts(output);
+}
+
+void decode(char *input) {
+
+    char output[1024] = {'\0'};
+    char * p = output;
+
+    while (*input) {
+        *p = *input - 3;
+        input++;
+        p++;
+    }
+    puts(output);
+}
