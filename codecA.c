fde560bc0746ddf6be5e5c7282af35a1eda7d60f
neryaRez <121443102+neryaRez@users.noreply.github.com>
2023-04-19 16:28:13 +0300

    Update codecA.c

--- codecA.c
+++ codecA.c
@@ -2,4 +2,27 @@
 Matala 2 in Operating systems
  by ilan Souffir and Nerya Rez
 */
+#include <ctype.h>
+#include <stdio.h>
+
+void encode(char *input) {
+    char output[1024] = {'\0'};
+    char * p = output;
+    while (*input) {
+        if (isupper(*input)) {
+            *p = tolower(*input);
+        } else if (islower(*input)) {
+            *p = toupper(*input);
+        } else {
+            *p = *input;
+        }
+        input++;
+        p++;
+    }
+    puts(output);
+}
+
+void decode(char *input) {
+    encode(input);
+}
 
