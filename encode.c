81ca30ffd8c8f7aaf31cfc9147040fa1402ea642
neryaRez <121443102+neryaRez@users.noreply.github.com>
2023-04-19 16:34:40 +0300

    Update encode.c

--- encode.c
+++ encode.c
@@ -1,4 +1,35 @@
-/* 
-Matala 2 in Operating systems
- by ilan Souffir and Nerya Rez
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Usage: encode <codec> <message>\n");
+        return 1;
+    }
+
+    void *lib_handle;
+    void (*encode_func)(char*);
+
+    char lib_name[256];
+    snprintf(lib_name, sizeof(lib_name), "%s%s", argv[1], ".so");
+
+    lib_handle = dlopen(lib_name, RTLD_NOW);
+    if (!lib_handle) {
+        printf("Error loading library: %s\n", dlerror());
+        return 1;
+    }
+
+    encode_func = dlsym(lib_handle, "encode");
+    if (!encode_func) {
+        printf("Error loading function: %s\n", dlerror());
+        dlclose(lib_handle);
+        return 1;
+    }
+
+    encode_func(argv[2]);
+
+    dlclose(lib_handle);
+    return 0;
+}
