8ad90475c9ef5ec2c912d82257498eac2e85129e
neryaRez <121443102+neryaRez@users.noreply.github.com>
2023-04-30 11:19:16 +0300

    Update stshell.c

--- stshell.c
+++ stshell.c
@@ -11,6 +11,7 @@ Matala 2 in Operating systems
 #include "unistd.h"
 #include <string.h>
 #include <signal.h>
+#include <stdbool.h>
 
 void handle_sigint(int sig) {
     // do nothing
@@ -23,13 +24,18 @@ int main(){
 	char command[1024];
 	char *token;
 
+	char* output_file;
+	bool redi1 = false;
+	bool redi2 = false;
+
+	int fd;
 
 
     signal(SIGINT, handle_sigint);
 
 
 	while (1) {
-	    printf("hello: ");
+	    printf("hello: \n");
 	    fgets(command, 1024, stdin);
 	    command[strlen(command) - 1] = '\0'; // replace \n with \0
 
@@ -38,10 +44,43 @@ int main(){
 	    token = strtok (command," ");
 	    while (token != NULL)
 	    {
+        
+		    if(strcmp(token, ">") == 0)
+		    {
+		    redi1 = true;
+		    output_file = strtok (NULL, " ");
+		    printf("the output file is: %s\n", output_file);
+		    break;
+		   } 
+			else if(strcmp(token, ">>") == 0)
+			{
+			redi2 = true;
+			output_file = strtok (NULL, " ");
+			printf("the output file is: %s\n", output_file);
+		    break;
+			} 
 		argv[i] = token;
+		printf (" argv[%d] == %s\n",i, argv[i]);
 		token = strtok (NULL, " ");
 		i++;
 	    }
+
+		// printf("i == %d\n", i);
+
+
+
+		// else if (redi2)
+		// {
+		// 	fd = fopen(argv[i-1], "ab");
+		// 	if(dup2(fileno(fd), STDOUT_FILENO) == -1){
+		// 		perror("cannot redirect");
+		// 		return 1;
+		// 	}
+		// 	fclose(fd);
+			
+		// }
+		
+		
 	    argv[i] = NULL;
 
 	    /* Is command empty */ 
@@ -56,10 +95,37 @@ int main(){
 
 	    /* for commands not part of the shell command language */ 
 	    if (fork() == 0) { 
+			if (redi1 || redi2)
+			{
+			if (redi1) fd = open(output_file, O_WRONLY|O_TRUNC|O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP, S_IROTH);
+			else if(redi2) fd = open(output_file, O_WRONLY| O_APPEND|O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP| S_IROTH);
+
+			// if(fd == NULL){
+			// 	perror("can't open the file\n");
+			// 	return 1;
+
+			// }
+
+			if(dup2(fd, STDOUT_FILENO) == -1){
+				perror("cannot redirect\n");
+				return 1;
+			}
+			if(dup2(fd, STDERR_FILENO) == -1){
+				perror("cannot redirect\n");
+				return 1;
+			}
+			
+			
+			// fflush(stdout);
+			}
+		if(redi1||redi2) close(fd);
 		execvp(argv[0], argv);
 		
+		 
 	    } 
         wait(NULL);
+		
+		// fclose(fd);
 
 	}
 
